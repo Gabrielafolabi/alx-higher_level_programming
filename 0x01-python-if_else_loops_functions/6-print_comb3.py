27524605552ba6245411a658dfda517247edfa73
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-11-29 11:22:31 -0800

    updatd

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}".format(i * 10 + j), end=", ")
-        if i == 9 and j == 8:
-            print("{}{}" .format(1 * 10 + j))
-            print("\n")
+        if i == 8 and j == 9:
+            print("{}{}" .format(i, j))
+        else:
+            print("{}{}".format(i,  j), end=", ")
