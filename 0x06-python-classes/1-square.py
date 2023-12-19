e2259e1f027e4787f3afcf83c56eb00c34c0749c
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-12-19 14:14:03 -0800

    updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+""" class Square that defines a square by: 
+(based on 0-square.py)"""
+
 class Square:
     """ Defining a class """
     def __init__(self, size):
         
         """Initializing the creation of 
         object with size attribute """
-
         self._size = size
