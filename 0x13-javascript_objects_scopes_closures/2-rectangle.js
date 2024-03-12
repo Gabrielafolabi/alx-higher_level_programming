d29f9c584752d7e2e74e939e01e937229ce862f0
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2024-03-12 04:43:20 -0700

    update

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+}
+module.exports = Rectangle;
