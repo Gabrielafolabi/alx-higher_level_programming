9b12caea39eb6fdd5e42c5f36e1d9a8ef137cf0e
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2024-03-22 12:25:05 -0700

    donenowoen

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// function that returns the reversed version of a list withouth reverse
-exports.esrever = function (list) {
-  return list.reduceRight(function (array, current) {
-    array.push(current);
-    return array;
-  }, []);
-};
