afe6433aa1fefcebb1e00bfbf569f0a1d3c69cb9
Gabriel Afolabi <35296784+Gabrielafolabi@users.noreply.github.com>
2024-03-23 09:43:08 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -34,7 +34,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -47,7 +47,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -60,7 +60,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -73,7 +73,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -85,7 +85,7 @@ class Rectangle(Base):
 
     def display(self):
         """Print the Rectangle using the `#` character."""
-        if self.width == 0 or self.height == 0:
+        if self.width is 0 or self.height is 0:
             print("")
             return
 
@@ -107,38 +107,38 @@ class Rectangle(Base):
                 - 5th argument represents y attribute
             **kwargs (dict): New key/value pairs of attributes.
         """
-        if args and len(args) != 0:
+        if args and len(args) is not 0:
             a = 0
             for arg in args:
-                if a == 0:
+                if a is 0:
                     if arg is None:
                         self.__init__(self.width, self.height, self.x, self.y)
                     else:
                         self.id = arg
-                elif a == 1:
+                elif a is 1:
                     self.width = arg
-                elif a == 2:
+                elif a is 2:
                     self.height = arg
-                elif a == 3:
+                elif a is 3:
                     self.x = arg
-                elif a == 4:
+                elif a is 4:
                     self.y = arg
                 a += 1
 
-        elif kwargs and len(kwargs) != 0:
+        elif kwargs and len(kwargs) is not 0:
             for k, v in kwargs.items():
-                if k == "id":
+                if k is "id":
                     if v is None:
                         self.__init__(self.width, self.height, self.x, self.y)
                     else:
                         self.id = v
-                elif k == "width":
+                elif k is "width":
                     self.width = v
-                elif k == "height":
+                elif k is "height":
                     self.height = v
-                elif k == "x":
+                elif k is "x":
                     self.x = v
-                elif k == "y":
+                elif k is "y":
                     self.y = v
 
     def to_dictionary(self):
